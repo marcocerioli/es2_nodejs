3c8dde4eecd04742aa00e53e97867d7bc243a997
Marco <ceriolimarco@itis-molinari.eu>
2025-02-08 18:29:30 +0100

    mod

---
+++ public/index.js
@@ -0,0 +1,38 @@
+
+const link = document.getElementById("link");
+const inputFile = document.getElementById('file');
+const button = document.getElementById("button");
+
+const render = async () => {
+    const list = await fetch("/filelist");
+    const data = await list.json();
+
+    const html = data
+        .map(e => `<li><a href="${e}">${e}</a></li>`)
+        .join("");
+
+    link.innerHTML = `<ul>${html}</ul>`; 
+};
+
+(async () => {
+    await render();
+})();
+
+const handleSubmit = async (event) => {
+    const formData = new FormData();
+    formData.append("file", inputFile.files[0]);
+    const body = formData;
+    const fetchOptions = {
+        method: 'post',
+        body: body
+    };
+    try {
+        const res = await fetch("/upload", fetchOptions);
+        const data = await res.json();
+        await render();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+button.onclick = handleSubmit;
